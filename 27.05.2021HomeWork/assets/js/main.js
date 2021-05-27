a81406c50a4aa938ff7c814251ea6780218cb2a8
RaminRzayev <raminag@code.edu.az>
2021-05-27 11:35:33 +0400

    HW

---
+++ 27.05.2021HomeWork/assets/js/main.js
@@ -0,0 +1,46 @@
+let arr = [43, 54, 65, 23, 64, 76];
+
+// FOREACH
+// arr.forEach(firstEach);
+// function firstEach(item){
+//     console.log(item**5);
+// }
+
+
+// FILTER
+// arr.filter(arrFilter);
+
+// function arrFilter(item){
+//     if(item>50){
+// console.log(item);
+//     }
+// }
+
+// MAP
+//   let newArr = arr.map(arrMap);
+// function arrMap(item){
+//     if(item%2==0){
+//         return item;
+//     }
+// }
+// console.log(newArr);
+
+// EVERY
+// let newArr=arr.every(newEvery);
+// function newEvery(item){
+//     if(item>5&&item<40){
+//         return true;
+//     }
+// }
+// console.log(newArr);
+
+
+// SOME
+// let newArr=arr.some(newEvery);
+// function newEvery(item){
+//     if(item>5&&item<40){
+//         return true;
+//     }
+// }
+// console.log(newArr);
+
